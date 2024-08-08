1fe6c0d2b219449822ac382cf6ea7780f1ad7cef
akashyap25 <anuragkashyap026@gmail.com>
2024-08-08 12:00:25 +0530

    cors test

--- index.js
+++ index.js
@@ -3,58 +3,62 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const expressSession = require("express-session");
+const expressSession = require('express-session');
 const authRoutes = require('./routes/authRoutes');
 const striperoutes = require('./routes/stripe-route');
-const MongoStore = require("connect-mongo");
+const MongoStore = require('connect-mongo');
 
-
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-const mongo_URI = process.env.MONGODB_URI; 
+const mongo_URI = process.env.MONGODB_URI;
 
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow requests from any origin
+    callback(null, true);
+  },
+  credentials: true // Allow credentials
+}));
 
-app.use(cors({ origin: true, credentials: true }));
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
-
-
-
-// express session
-app.use(
-    expressSession({
-      name: "session",
-      secret: process.env.JWT_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({ mongoUrl: mongo_URI }),
-      cookie: {
-        secure: false,
-        maxAge: 1000 * 60 * 60,
-        httpOnly: true,
-      },
-    })
-  );
-  
-app.use("/api/users", authRoutes); 
-app.use("/api/stripe", striperoutes); 
-
-
-
-
-mongoose.connect(mongo_URI, {  })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-        console.error('MongoDB connection error:', err);
-    });
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Middleware to set Access-Control-Allow-Credentials header
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
 });
 
+// Express session
+app.use(
+  expressSession({
+    name: 'session',
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: mongo_URI }),
+    cookie: {
+      secure: false, // Set to true if using HTTPS
+      maxAge: 1000 * 60 * 60, // 1 hour
+      httpOnly: true
+    }
+  })
+);
+
+app.use('/api/users', authRoutes);
+app.use('/api/stripe', striperoutes);
+
+mongoose.connect(mongo_URI, {})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
